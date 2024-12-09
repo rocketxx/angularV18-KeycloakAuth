904e9216b346839a2ca93514ae8f5d2269a69a77
rocketxx <william97.ragusa@gmail.com>
2024-12-09 21:48:06 +0100

    aggiunte info utente in pagina admin

--- src/app/components/admin/home-admin-role/home-admin-role.component.ts
+++ src/app/components/admin/home-admin-role/home-admin-role.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthUserService } from '../../../services/auth-user.service';
 
 @Component({
   selector: 'app-home-admin-role',
   templateUrl: './home-admin-role.component.html',
   styleUrl: './home-admin-role.component.scss'
 })
-export class HomeAdminRoleComponent {
+export class HomeAdminRoleComponent implements OnInit {
+  
+  userProfile: any | null = null;
+
+  constructor(private auth : AuthUserService){}
+  
+  async ngOnInit(): Promise<void> {
+    this.userProfile = await this.auth.loadUserProfile();
+  }
 
 }
